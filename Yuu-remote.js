2ba958d5858caece6b1139e68122c285789bdc6f
Scott Wu <scott.wu.hjx@gmail.com>
2016-03-04 21:23:24 -0500

    Updated packages to latest version.

--- Yuu-remote.js
+++ Yuu-remote.js
@@ -1,8 +1,14 @@
+// Packages
+var sio = require("socket.io")();
+var rjs = require("robotjs");
+var argv = require("minimist")(process.argv.slice(2));
+
 // Options
-// TODO Parse from the command line
-var SERVER_PORT = 988;
-var VERBOSITY = 20;
-var AUTH_KEY = "yuu";
+var HTTP_SERVER_PORT = argv["http-port"] || 3000;
+var HTTP_SERVER_ENABLED = argv["http-server"] || false;
+var WEBSOCKET_SERVER_PORT = argv["ws-port"] || 988;
+var VERBOSITY = argv["v"] || argv["verbosity"] || 20;
+var AUTH_KEY = argv["key"] || "yuu";
 
 var log = function(m, l) {
     if (VERBOSITY >= l) {
@@ -10,13 +16,9 @@ var log = function(m, l) {
     }
 };
 
-// Libraries
-var sio = require("socket.io")();
-var rjs = require("robotjs");
-
 // Setup robotjs
-rjs.setMouseDelay(2);
-rjs.setKeyboardDelay(2);
+rjs.setMouseDelay(0);
+rjs.setKeyboardDelay(0);
 
 // Setup socket.io
 sio.on("connection", function(socket) {
@@ -33,23 +35,49 @@ sio.on("connection", function(socket) {
             log("auth : " + sid, 10);
             
             // Now attach listeners
-            socket.on("msm", function(action) {
+            
+            // Mouse Absolute
+            socket.on("msa", function(coords) {
+                log("msa : " + sid + " : " + coords, 40);
+                rjs.moveMouse(coords.x, coords.y);
+            });
+            
+            // Mouse Relative
+            socket.on("msr", function(coords) {
                 
             });
             
+            // Keyboard down
             socket.on("kbd", function(key) {
                 log("kbd : " + sid + " : " + key, 30);
                 rjs.keyToggle(key, "down");
             });
             
+            // Keyboard up
             socket.on("kbu", function(key) {
                 log("kbu : " + sid + " : " + key, 30);
                 rjs.keyToggle(key, "up");
+            });
+            
+            // Keyboard type
+            socket.on("kbt", function(key) {
+                log("kbt : " + sid + " : " + key, 30);
+                rjs.keyTap(key);
+            });
+            
+            // Keyboard raw (supports modifiers)
+            socket.on("kbr", function(args) {
+                rjs.keyToggle.call(rjs, args);
+            });
+            
+            // Screen size
+            socket.on("ss", function() {
+                socket.emit("ss", rjs.getScreenSize());
             });
         }
     });
 });
 
-sio.listen(988);
+sio.listen(WEBSOCKET_SERVER_PORT);
 
-console.log("Listening on 988");
+log("Listening on " + WEBSOCKET_SERVER_PORT, 5);
